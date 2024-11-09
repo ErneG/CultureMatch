1918339e7a9b5a0b90a05e6b01f88fc4bf2ae0cf
Renārs Laicāns <rlaicans@gmail.com>
2024-11-09 14:52:57 +0200

    db models

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from . import models
 
 bp = Blueprint('main', __name__)
 
@@ -7,3 +8,11 @@ def get_users():
     return jsonify({
         "hello": "world"
     })
+
+
+# @bp.route('/company', methods=['GET'])
+# def get_companies():
+#     companies = models.Entity.query.all()
+#     return jsonify({
+#         "companies": [company.to_dict() for company in companies]
+#     })
